502582578c3bc2bd98e4c11de470fe1cf3661cd3
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2019-06-12 20:11:36 +0800

    task1 complete

--- Task1.py
+++ Task1.py
@@ -18,3 +18,13 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+different_telephone_number = set()
+for i in range(len(texts)):
+    different_telephone_number.add(texts[i][0])
+    different_telephone_number.add(texts[i][1])
+
+for i in range(len(calls)):
+    different_telephone_number.add(calls[i][0])
+    different_telephone_number.add(calls[i][1])
+
+print("There are {0} different telephone numbers in the records".format(len(different_telephone_number)))
